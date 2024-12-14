6bcc5620430a0da5a16ddb4fa3e7988fe0e953e5
Inna Turetska <innaturetska1987@gmail.com>
2024-12-14 10:54:33 +0200

    corretc task 6

--- js/task-6.js
+++ js/task-6.js
@@ -4,35 +4,36 @@ function getRandomHexColor() {
     .padStart(6, 0)}`;
 }
  const box = document.querySelector("#boxes");
-  const btnCreate = document.querySelector(".btnCreate");
-  const btnDestroy = document.querySelector(".btnDestroy");
-  const inpNumber = document.querySelector(".inpNumber");
+ const btnCreate = document.querySelector(".btnCreate");
+ const btnDestroy = document.querySelector(".btnDestroy");
+ const inpNumber = document.querySelector(".inpNumber");
 
 
-    const createSquares = () => {
-      const numSquares = parseInt(inpNumber.value);
-      if (numSquares > 0 && numSquares <= 100) {
-        box.innerHTML = "";
-        
+  const createSquares = () => {
+  const numSquares = parseInt(inpNumber.value);
+    
+    if (numSquares > 0 && numSquares <= 100) {
+    const fragment = document.createDocumentFragment();
+    
+    for (let i = 0; i < numSquares; i++) {
+      const square = document.createElement("div");
+      square.classList.add("square");
+      const size = 30 + i * 10; 
+      square.style.width = `${size}px`;
+      square.style.height = `${size}px`;
+      square.style.backgroundColor = getRandomHexColor(); 
+      fragment.appendChild(square);
+    }
+    
+    box.innerHTML = "";
+    box.appendChild(fragment);
+  } 
+};
 
-        for (let i = 0; i < numSquares; i++) {
-          const square = document.createElement("div");
-          square.classList.add("square");
-          const size = 30 + i * 10; 
-          square.style.width = `${size}px`;
-          square.style.height = `${size}px`;
-          square.style.backgroundColor = getRandomHexColor();
-          box.append(square);
-        }
-      } 
-    };
+  const destroySquares = () => {
+   box.innerHTML = ""; 
+   inpNumber.value = ""; 
+};
 
-   
-    const destroySquares = () => {
-      box.innerHTML = "";
-      inpNumber.value = "";
-    };
-
-
-    btnCreate.addEventListener("click", createSquares);
-    btnDestroy.addEventListener("click", destroySquares);
+btnCreate.addEventListener("click", createSquares);
+btnDestroy.addEventListener("click", destroySquares);
